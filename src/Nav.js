6879a0a3d63dced8eb60a0e4154f8b1b407ebf80
Jairo Perdomo <jaanpeto_wx@hotmail.com>
2020-08-01 21:19:15 -0500

    project netflix

---
+++ src/Nav.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Logo_Netflix.png/1024px-Logo_Netflix.png"
+        alt="Netflix Logo"
+      ></img>
+    </div>
+  );
+}
+
+export default Nav;
